ecf91c87650bd16bf033769af882534f1a72c475
Aryan <aryansharma150074@gmail.com>
2024-03-01 00:54:31 +0530

    api updated

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -19,13 +19,13 @@ const RestaurantMenu = () => {
   }
 
   const { name, cloudinaryImageId, areaName } =
-    menu?.cards[2]?.card?.card?.info;
+    menu?.cards[0]?.card?.card?.info;
 
   // const { itemCards } =
   //   menu?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
   const categories =
-    menu?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+    menu?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c.card?.["card"]?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
